e1970cdeece8581e22e2521e1d734f8bba6efccd
Vrajesh Modi <91773413+VRAJESHMODI-30@users.noreply.github.com>
2022-04-14 01:53:01 +0530

    Created endpoint for login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const JWT_KEY = "SECRET-KEY";
 
-//Create a User using POST "/api/auth/signup". ~ Basically for SignUp.
+//ROUTE-1: Create a User using POST "/api/auth/signup". ~ Basically endpoint for SignUp.
 router.post(
   "/signup",
   [
@@ -29,6 +29,7 @@ router.post(
           .json({ error: "This email already has been registered...!" });
       }
 
+      //Hashing password
       let salt = await bcrypt.genSalt(10);
       let hashPass = await bcrypt.hash(req.body.password, salt);
 
@@ -38,6 +39,52 @@ router.post(
         email: req.body.email,
         password: hashPass,
       });
+      //Creating authtoken for user
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      let authtoken = jwt.sign(data, JWT_KEY);
+      res.json({ authtoken: authtoken });
+    } catch (error) {
+      console.error(error);
+      res.status(500).res.json({ err: "Internal Server Error...!" });
+    }
+  }
+);
+
+//ROUTE-2:Login a User using POST "/api/auth/signup". ~ Basically endpoint for LogIn.
+router.post(
+  "/login",
+  [
+    body("email", "Username Invalid").isEmail(),
+    body("password", "password can not be blank").exists(),
+  ],
+  async (req, res) => {
+    //If validation get compromissed then error occur else move forward.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      //Check email:
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please login with correct credentials" });
+      }
+
+      //Check password:
+      let comparePassword = await bcrypt.compare(password, user.password);
+      if (!comparePassword) {
+        return res
+          .status(400)
+          .json({ error: "Please login with correct credentials" });
+      }
+
       //Creating authtoken for user
       const data = {
         user: {
